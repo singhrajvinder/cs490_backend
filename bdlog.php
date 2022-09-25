e9207d05dfd9da6adb7534b1e664366c8929f117
Rajvinder Singh <70456872+singhrajvinder@users.noreply.github.com>
2022-09-25 17:31:43 -0400

    Update bdlog.php

--- bdlog.php
+++ bdlog.php
@@ -1,8 +1,8 @@
 <?php
    //database connection
    $server = 'sql1.njit.edu';
-   $dbuser = 'rs2264';
-   $pass = 'Sin@913782465';
+   $dbuser = '******';
+   $pass = '*************';
    $dbname = 'rs2264';
    $database = mysqli_connect($server, $dbuser, $pass, $dbname);
    if(mysqli_connect_errno()){
